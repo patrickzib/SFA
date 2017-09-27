d4f124f55d4def0062ca0aae6dd2075c001bd041
Patrick Schäfer <patrick.schaefer@zib.de>
2017-09-27 16:04:21 +0200

    updated accuracies

--- src/test/java/sfa/classification/WEASELClassifierTest.java
+++ src/test/java/sfa/classification/WEASELClassifierTest.java
@@ -9,10 +9,10 @@ public class WEASELClassifierTest extends AbstractClassifierTest{
         List<DataSet> dataSets=new ArrayList<>();
         //Coffee;Weasel;1.0;1.0
         dataSets.add(new DataSet("Coffee", 1.0, 1.0));
-        //Beef;Weasel;0.867;0.833
-        dataSets.add(new DataSet("Beef", 0.867, 0.833));
-        //CBF;Weasel;1.0;0.984
-        dataSets.add(new DataSet("CBF", 1.0, 0.984));
+        //Beef;Weasel;0.833;0.833
+        dataSets.add(new DataSet("Beef", 0.833, 0.833));
+        //CBF;Weasel;0.967;0.988
+        dataSets.add(new DataSet("CBF", 1.0, 0.988));
         return dataSets;
     }
 
