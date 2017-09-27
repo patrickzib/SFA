eefdde2476ce0f1d17665fb49e3b7c2ccffc0915
Patrick Schäfer <patrick.schaefer@zib.de>
2017-09-27 16:05:12 +0200

    updated accuracies

--- src/test/java/sfa/classification/BossEnsembleClassifierTest.java
+++ src/test/java/sfa/classification/BossEnsembleClassifierTest.java
@@ -9,7 +9,7 @@ public class BossEnsembleClassifierTest extends AbstractClassifierTest{
         List<DataSet> dataSets=new ArrayList<>();
         //Coffee;BOSS Ensemble;1.0;1.0
         dataSets.add(new DataSet("Coffee", 1.0, 1.0));
-        //Beef;BOSS Ensemble;1.0;0.733
+        //Beef;BOSS Ensemble;1.0;0.833
         dataSets.add(new DataSet("Beef", 1.0, 0.833));
         //CBF;BOSS Ensemble;1.0;0.999
         dataSets.add(new DataSet("CBF", 1.0, 0.999));
